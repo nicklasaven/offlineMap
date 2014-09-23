3e2b50d90a2419c6bb13596eb7227cef0726243d
nicklas <nicklas.aven@jordogskog.no>
2014-09-23 13:51:11 +0200

    seems like hanging IndexedDB loading is fixed with an ugly hack

--- attributes_worker.js
+++ attributes_worker.js
@@ -1,17 +1,38 @@
 
 const dbName = "the_db";
 var db;
-
+var dbName;
 function add_attributes(the_name, the_data)
 {
-var n=0;
-	    // Store values in the newly created objectStore.
-	    var attributeStore = db.transaction([the_name], "readwrite").objectStore(the_name);
-	    for (var i=0,len =the_data.length;i<len;i++)
-	    {
-	      attributeStore.put(the_data[i]).onsuccess=function(e){if (++n==len) postMessage({"n":n});};
-	    }
+	var attributeStore = db.transaction([the_name], "readwrite").objectStore(the_name);
+	var n=0,i=0;
+	var len=the_data.length;
+	/*Warning, here comes an ugly hack. It seems like it can't handle putting all rows in que for writing to indexeDB. But is also gets extreamly slow to only start a new writing process on previous onsucess event. In this way 3 writing processs is in the que.
+	 That is much easier to handle for the database than 300000 but aslo kicks a lot better than one by one.
+	 IndexedDB is not an easy animal to get to know.*/
+	putNext();
+	putNext();
+	putNext();
+	    
+	function putNext() 
+	{
+		if (i<len) 
+		{
+			n++;
+			attributeStore.put(the_data[i++]).onsuccess = putNext;
+			if(n>=20000)
+			{
+				postMessage({"type":"finnished","msg":i+" rows are loaded of "+len+ " , still working ...."});
+				n=0;
+			}
+		} 
+		else 
+		{   // complete
+			postMessage({"type":"finnished","msg":"Finnished loading "+i+" rows"});
+		}
+	}
 }
+
 
 
 
@@ -42,19 +63,56 @@ function processData(csv,the_name) {
 
 
 self.addEventListener('message', function(e) {
-  var the_file = e.data.the_file;
-  var the_name = e.data.the_name;
-var dbrequest = indexedDB.open(dbName);
-	dbrequest.onsuccess=function(e){
-	 db = e.target.result;
-	 
-  // Read each file synchronously as an ArrayBuffer and
-  // stash it in a global array to return to the main app.
+	var the_files = e.data.the_files;
+	var next_version = e.data.next_version;
+	var layers=[];
+	dbName=e.data.dbName;
+ 
+	var dbrequest = indexedDB.open(dbName,next_version++);
+	postMessage({"type":"version","next_version":next_version});
+	dbrequest.onerror = function(event) 
+	{
+		postMessage({"type":"error","msg":"Problems opening data base: " + event.target.errorCode});
+	};
+	dbrequest.onblocked = function(event) {
+		postMessage({"type":"error","msg":"The database is blocked. Close the map if it is open"});
+	};
+
+	dbrequest.onupgradeneeded = function(event) {
+		db = event.target.result;
+
+		for (var f=0,len=the_files.length;f<len;f++)
+		{
+			var the_name = the_files[f].name.split(".")[0];
+			layers.push({"the_name":the_name, "the_file":the_files[f]});
+			if(db.objectStoreNames.contains(the_name)) 
+			{
+				postMessage({"type":"messege","msg":"Deleting old dataset for '"+the_name+"'"});
+				db.deleteObjectStore(the_name);
+			}
 				
-   var reader = new FileReaderSync();
- processData(reader.readAsText(the_file),the_name);
+			var objectStore = db.createObjectStore(the_name, { keyPath: "gid" });  
+				objectStore.transaction.oncomplete=function (e) {		
+				console.log("Ok, we are here");
+				}
+		}	
+	}
+  
+  	dbrequest.onsuccess = function(event) {
+		for (var f=0,len=layers.length;f<len;f++)
+		{
+			
+			  var reader = new FileReaderSync();
+			theBlob=reader.readAsText(layers[f].the_file)
+			processData(theBlob,layers[f].the_name);
+			theBlob.delete;
+			
+
+		}
 		
-		
+	};
+	
+	
 	self.close;
-	 }
+	 
 }, false);
